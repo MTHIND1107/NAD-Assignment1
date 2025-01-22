16cf0f05cbb160881de53bfbc98c68527dcaecd5
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-01-21 19:13:09 -0500

    Added file handling functionality for loading, saving, sending, receiving and verifying files

---
+++ ReliableUDP/fileHandler.cpp
@@ -0,0 +1,24 @@
+#include "fileHandler.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int LoadFile(void) {
+
+}
+
+int SaveFile(void) {
+
+}
+
+int SendFile(void) {
+
+}
+
+int ReceiveFile(void) {
+
+}
+
+int VerifyFile(void) {
+
+}
---
+++ ReliableUDP/fileHandler.h
@@ -0,0 +1,15 @@
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+#include <stdbool.h>
+
+int LoadFile(void);
+
+int SaveFile(void);
+
+int SendFile(void);
+
+int ReceiveFile(void);
+
+int VerifyFile(void);
+
+#endif
