83e1c289c0c796402dc0a1fe647ca7c08d7a7393
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2025-02-15 03:08:31 -0500

    Prototypes added

--- ReliableUDP/fileHandler.h
+++ ReliableUDP/fileHandler.h
@@ -1,19 +1,20 @@
 #ifndef FILE_HANDLER_H
 #define FILE_HANDLER_H
+#pragma once
 #include <stdbool.h>
 #include <stdint.h>
 
 #define PACKET_SIZE 1024
 #define CHECKSUM_SIZE 4  // CRC32 checksum size
 
-
+void init_crc32_table(void);
+uint32_t computeCRC32(const char* data, size_t size);
 int loadFile(const char* filename, char** buffer, size_t* size);
 int saveFile(const char* filename, const char* buffer, size_t size);
 double calculateTransferSpeed(double startTime, double endTime, size_t fileSize);
-
-int SendFile(const char* filename, const char* destIP, int destPort);
-
-int ReceiveFile(const char* outputFilename, int listenPort);
+void createMetadataPacket(const char* filename, size_t fileSize, uint32_t crc, bool isLast, char* packet, size_t* packetSize);
+bool extractMetadataPacket(const char* packet, FileMetadata* metadata);
+size_t createDataPacket(const char* fileBuffer, size_t fileSize, size_t currentOffset, char* tempBuffer, size_t maxPacketSize, bool isLastPacket);
 
 bool VerifyFile(const char* filename, uint32_t expectedCRC);
 
