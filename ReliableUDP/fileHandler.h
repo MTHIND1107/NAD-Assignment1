877b0379386845f2ddf487b68eeebd1809fc82ad
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2025-02-14 22:47:19 -0500

    Small changes in .h file

--- ReliableUDP/fileHandler.h
+++ ReliableUDP/fileHandler.h
@@ -6,6 +6,7 @@
 #define PACKET_SIZE 1024
 #define CHECKSUM_SIZE 4  // CRC32 checksum size
 
+
 int loadFile(const char* filename, char** buffer, size_t* size);
 int saveFile(const char* filename, const char* buffer, size_t size);
 double calculateTransferSpeed(double startTime, double endTime, size_t fileSize);
